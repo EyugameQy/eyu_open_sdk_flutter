a32b96956a1b811da39fa342f330fe0221db8a3b
wuxiaowei <wuxiaowei916@gmail.com>
2021-06-22 10:39:19 +0800

    first commit

---
+++ example/android/app/src/main/kotlin/com/eyu/eyu_open_sdk_flutter_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eyu.eyu_open_sdk_flutter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Classes/EyuOpenSdkFlutterPlugin.m
@@ -0,0 +1,15 @@
+#import "EyuOpenSdkFlutterPlugin.h"
+#if __has_include(<eyu_open_sdk_flutter/eyu_open_sdk_flutter-Swift.h>)
+#import <eyu_open_sdk_flutter/eyu_open_sdk_flutter-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "eyu_open_sdk_flutter-Swift.h"
+#endif
+
+@implementation EyuOpenSdkFlutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftEyuOpenSdkFlutterPlugin registerWithRegistrar:registrar];
+}
+@end
