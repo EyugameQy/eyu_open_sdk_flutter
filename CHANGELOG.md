528e7b55f004e8d694bea22a31f0df0f602fc44d
wuxiaowei <wuxiaowei916@gmail.com>
2021-07-07 14:54:42 +0800

    合并代码

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 ## 0.0.1
+内部使用
+* 集成了Android sdk，支持激励视频、插屏、banner和native广告
 
-* TODO: Describe initial release.
+## 0.0.2
+集成了iOS sdk，支持激励视频、插屏、banner和native广告
--- lib/src/ad_listeners.dart
+++ lib/src/ad_listeners.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'package:meta/meta.dart';
-
 import 'ad_containers.dart';
 
 typedef AdEventCallback = void Function(EyuAd ad);
